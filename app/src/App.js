b4a56647574f852d5cee4475245041fe1fe22e6b
Marco Bruno <marco.bruno.br@gmail.com>
2019-03-27 14:56:28 +0000

    Crio o component BtnBotcamp de vez e de forma isolada e feliz

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import "./App.css"
+import BtnBotcamp from "./components/BtnBotcamp";
 
 class App extends Component {
     render() {
-        return <button className="btn-botcamp">Entrar</button>
+        return <BtnBotcamp />
     }
 }
 
-export default App
+export default App;
---
+++ app/src/components/BtnBotcamp/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css"
+
+const BtnBotcamp = () => (
+    <button className="btn-botcamp">Entrar</button>
+);
+
+export default BtnBotcamp;
