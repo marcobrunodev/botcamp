2da8e73038c0a4a0f5e51273a00b719c680788b9
Marco Bruno <marco.bruno.br@gmail.com>
2019-03-27 14:23:00 +0000

    Criamos o component App

---
+++ app/src/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class App extends Component {
+    render() {
+        return <button>Entrar</button>
+    }
+}
+
+ReactDOM.render( <App /> ,document.getElementById("root"))
