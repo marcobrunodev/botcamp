4264e51264f5eaa607d0e46c8d27b85305d9ac2e
Marco Bruno <marco.bruno.br@gmail.com>
2019-03-27 14:44:13 +0000

    Adicionar o CSS no App.js

---
+++ app/src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./App.css"
+
+class App extends Component {
+    render() {
+        return <button className="btn-botcamp">Entrar</button>
+    }
+}
+
+export default App
--- app/src/index.js
+++ app/src/index.js
@@ -1,10 +1,6 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 
-class App extends Component {
-    render() {
-        return <button>Entrar</button>
-    }
-}
+import App from "./App"
 
-ReactDOM.render( <App /> ,document.getElementById("root"))
+ReactDOM.render(<App /> ,document.getElementById("root"))
