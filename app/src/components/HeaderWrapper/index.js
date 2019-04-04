1ea86fef5b37381e641fb946b2c123ccc7e31fd1
Marco Bruno <marco.bruno.br@gmail.com>
2019-04-04 13:22:39 +0000

    Aula04 47 Criar o component HeaderWrapper

---
+++ app/src/components/HeaderWrapper/index.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const HeaderWrapper = styled.header`
+  background-color: #26265e;
+`;
+
+export default HeaderWrapper;
--- app/src/pages/Chat/index.js
+++ app/src/pages/Chat/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 
+import HeaderWrapper from "../../components/HeaderWrapper";
 import LogoBotcamp from "../../components/LogoBotcamp";
 import LogoutBotcamp from "../../components/LogoutBotcamp";
 
 const Chat = () => (
-  <>
+  <HeaderWrapper>
     <LogoBotcamp />
     <LogoutBotcamp />
-  </>
+  </HeaderWrapper>
 );
 export default Chat;
