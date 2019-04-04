d0e336241f580c104dd33e5fb44df3a0ba217aaa
Marco Bruno <marco.bruno.br@gmail.com>
2019-04-04 13:13:55 +0000

    Aula04 46 Criar o  component  LogoutBotcamp

---
+++ app/src/components/LogoutBotcamp/index.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+import logout from "../../img/logout.png";
+
+const LogoutBotcamp = styled.img.attrs({
+  src: logout,
+  alt: "Icone para sair"
+})``;
+
+export default LogoutBotcamp;
--- app/src/pages/Chat/index.js
+++ app/src/pages/Chat/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 import LogoBotcamp from "../../components/LogoBotcamp";
+import LogoutBotcamp from "../../components/LogoutBotcamp";
 
-const Chat = () => <LogoBotcamp />;
-
+const Chat = () => (
+  <>
+    <LogoBotcamp />
+    <LogoutBotcamp />
+  </>
+);
 export default Chat;
