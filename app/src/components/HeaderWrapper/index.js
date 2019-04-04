8c257f76dd35d5834c898aa2da2b1eb025f4f947
Marco Bruno <marco.bruno.br@gmail.com>
2019-04-04 14:07:06 +0000

    Aula04 50 Diminuir as coisas do HeaderWrapper

--- app/src/components/HeaderWrapper/index.js
+++ app/src/components/HeaderWrapper/index.js
@@ -3,6 +3,8 @@ import styled from "styled-components";
 const HeaderWrapper = styled.header`
   background-color: #26265e;
   padding: 10px 16px;
+  display: flex;
+  justify-content: space-between;
 `;
 
 export default HeaderWrapper;
