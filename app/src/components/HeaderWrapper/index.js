ba8c9046bf9ba82e60f292b108fad937beb3e60d
Marco Bruno <marco.bruno.br@gmail.com>
2019-04-09 19:39:45 +0000

    Aula05 55 Adicionar diagonal no HeaderWrapper

--- app/src/components/HeaderWrapper/index.js
+++ app/src/components/HeaderWrapper/index.js
@@ -5,6 +5,19 @@ const HeaderWrapper = styled.header`
   padding: 10px 16px;
   display: flex;
   justify-content: space-between;
+
+  ::after {
+    content: "";
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    border: 50px solid;
+    border-top-color: blue;
+    border-right-color: transparent;
+    border-bottom-color: transparent;
+    border-left-color: transparent;
+  }
 `;
 
 export default HeaderWrapper;
