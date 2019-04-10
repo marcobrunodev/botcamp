651c5600310052aaf6af597262c19e808c83ec1f
Marco Bruno <marco.bruno.br@gmail.com>
2019-04-10 11:45:49 +0000

    Aula05 60 Config do react-router-dom

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import Router from "./Router";
+import Login from "./pages/Login";
+import Chat from "./pages/Chat";
 
 import "./styles/reset.css";
 
 class App extends Component {
   render() {
-    return <Router />;
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Login} />
+          <Route path="/chat" component={Chat} />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
