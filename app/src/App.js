b69909732a02d90ecd82c22d4159f3aa12e21296
Marco Bruno <marco.bruno.br@gmail.com>
2019-05-15 14:00:25 +0100

    Aula06 67 Redirect para o Login

--- app/src/App.js
+++ app/src/App.js
@@ -12,7 +12,8 @@ class App extends Component {
       <BrowserRouter>
         <Switch>
           <Route path="/" exact component={Login} />
-          <Route path="/chat" component={Chat} />
+          <Route path="/chat" exact component={Chat} />
+          <Route paht="*" component={Login} />
         </Switch>
       </BrowserRouter>
     );
